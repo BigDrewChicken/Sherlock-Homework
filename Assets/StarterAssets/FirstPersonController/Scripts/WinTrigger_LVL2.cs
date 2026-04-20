cb03170b1b670fd5f142858108537880f9d1ce50
CHARLESYEXELPALOMARES <qcyapalomares@tip.edu.ph>
2026-04-20 16:57:29 +0800

    final rework

---
+++ Assets/StarterAssets/FirstPersonController/Scripts/WinTrigger_LVL2.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using VIDE_Data;
+
+public class WinTrigger_LVL2 : MonoBehaviour
+{
+    public GameObject winCanvas;
+    // Set this to exactly what your Culprit's name is in the Hierarchy
+    public string culpritName = "Defendant 3";
+
+    void OnEnable()
+    {
+        VD.OnEnd += HandleDialogueEnd;
+    }
+
+    void OnDisable()
+    {
+        VD.OnEnd -= HandleDialogueEnd;
+    }
+
+    void HandleDialogueEnd(VD.NodeData data)
+    {
+        // THE SAFETY FILTER:
+        // We check the name of the 'nodeData.tag' or the object we are interacting with.
+        // If the dialogue that just ended isn't from our Culprit, we stop here.
+        if (data.tag != culpritName)
+        {
+            return;
+        }
+
+        // Only if it's the culprit do we show the win screen
+        if (winCanvas != null)
+        {
+            winCanvas.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Debug.Log("Success: " + culpritName + " confessed. Win screen shown.");
+        }
+    }
+}
