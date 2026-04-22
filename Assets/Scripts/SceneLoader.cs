0b9a26735c128fb5f9b6bee18087e8191c0c18aa
RossUsesGit <rossbulaong@gmail.com>
2026-04-23 05:04:17 +0800

    Final-final build, the one I uploaded on Unity WebGL

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,35 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-#if UNITY_EDITOR // Everything that is inside this clause only runs when in the Unity Editor. 
-using UnityEditor;
-#endif
-
 public class SceneLoader : MonoBehaviour
 {
-    // Scene dragged from the Project window to avoid typing scene names manually
-#if UNITY_EDITOR // Scene asset can only be accessed in Unity Editor, so this is essential.
-    public SceneAsset scene;
-#endif
+    public string sceneName;
 
-    // Stores the scene name used by SceneManager
-    private string sceneName;
-
-    // Gets the scene name from the assigned SceneAsset
-    void Awake()
-    {
-#if UNITY_EDITOR // The whole purpose of finding the SceneAsset name is to store it in a variable so the function can load the scene based on the scene name 
-        sceneName = scene.name;
-#endif
-    }
-    
-    // Loads the assigned scene (used for level buttons)
     public void LoadScene()
     {
         SceneManager.LoadScene(sceneName);
     }
-
-
-    
-
 }
